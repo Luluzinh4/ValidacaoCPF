6e4595c3e4c23bec017f518a080655a61e06194c
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2020-03-21 20:24:35 -0300

    First Commit

---
+++ ValidacaoCPF/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ValidacaoCPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultadoValidacao = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // resultadoValidacao
+            // 
+            this.resultadoValidacao.AutoSize = true;
+            this.resultadoValidacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultadoValidacao.Location = new System.Drawing.Point(146, 145);
+            this.resultadoValidacao.Name = "resultadoValidacao";
+            this.resultadoValidacao.Size = new System.Drawing.Size(82, 20);
+            this.resultadoValidacao.TabIndex = 0;
+            this.resultadoValidacao.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(109, 91);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(130, 20);
+            this.txtCpf.TabIndex = 2;
+            this.txtCpf.TextChanged += new System.EventHandler(this.txtCpf_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite o CPF:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Validação de CPF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 205);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultadoValidacao);
+            this.Name = "Form1";
+            this.Text = "Validador de CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label resultadoValidacao;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ValidacaoCPF/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidacaoCPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cpf = txtCpf.Text;
+
+            if(cpf.Length != 11)
+            {
+                MessageBox.Show("Insira um valor com 11 dígitos", "Problema!",
+                    MessageBoxButtons.OK);
+                txtCpf.Focus();
+                return;
+            }
+
+            if (cpf.Equals("11111111111") || cpf.Equals("22222222222") ||
+                cpf.Equals("33333333333") || cpf.Equals("44444444444") ||
+                cpf.Equals("55555555555") || cpf.Equals("66666666666") ||
+                cpf.Equals("77777777777") || cpf.Equals("88888888888") ||
+                cpf.Equals("99999999999"))
+            {
+                resultadoValidacao.Text = "CPF Inválido";
+                return;
+            }
+
+            bool isNumeric = true;
+            char[] charsCpf = cpf.ToCharArray();
+
+            foreach(var datachar in charsCpf)
+            {
+                isNumeric = isNumeric ? char.IsDigit(datachar) : false;
+            }
+
+            if (!isNumeric)
+            {
+                MessageBox.Show("Insira um valor numérico", "Problema!",
+                    MessageBoxButtons.OK);
+                txtCpf.Focus();
+                return;
+            }
+
+            short digito1 = short.Parse(charsCpf[0].ToString());
+            short digito2 = short.Parse(charsCpf[1].ToString());
+            short digito3 = short.Parse(charsCpf[2].ToString());
+            short digito4 = short.Parse(charsCpf[3].ToString());
+            short digito5 = short.Parse(charsCpf[4].ToString());
+            short digito6 = short.Parse(charsCpf[5].ToString());
+            short digito7 = short.Parse(charsCpf[6].ToString());
+            short digito8 = short.Parse(charsCpf[7].ToString());
+            short digito9 = short.Parse(charsCpf[8].ToString());
+            short digito10 = short.Parse(charsCpf[9].ToString());
+            short digito11 = short.Parse(charsCpf[10].ToString());
+
+            int soma9Digitos = (digito1 * 10) + (digito2 * 9) + (digito3 * 8) +
+                (digito4 * 7) + (digito5 * 6) + (digito6 * 5) + (digito7 * 4) +
+                (digito8 * 3) + (digito9 * 2);
+
+            int resultado1 = (soma9Digitos * 10) % 11;
+
+            resultado1 = resultado1 != 10 ? resultado1 : resultado1 = 0;
+
+            if(resultado1 != digito10)
+            {
+                resultadoValidacao.Text = "CPF Inválido";
+            }
+            else
+            {
+                int soma10Digitos = (digito1 * 11) + (digito2 * 10) + (digito3 * 9) +
+                    (digito4 * 8) + (digito5 * 7) + (digito6 * 6) + (digito7 * 5) +
+                    (digito8 * 4) + (digito9 * 3) + (digito10 * 2);
+
+                int resultado2 = (soma10Digitos * 10) % 11;
+
+                resultado2 = resultado2 != 10 ? resultado2 : resultado2 = 0;
+
+                if(resultado2 != digito11)
+                {
+                    resultadoValidacao.Text = "CPF Inválido";
+                }
+                else
+                {
+                    resultadoValidacao.Text = "CPF Válido";
+                }
+            }
+
+        }
+
+        private void txtCpf_TextChanged(object sender, EventArgs e)
+        {
+            resultadoValidacao.Text = "Resultado";
+        }
+    }
+}
